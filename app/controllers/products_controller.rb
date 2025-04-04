59c42223d2f37c609c5e50b2835540f6dcbedc56
Luis Bonilla <bonillasv@outlook.com>
2025-04-04 07:55:38 -0700

    Cloudinary funcionando

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -45,7 +45,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :price, :description)
+    params.require(:product).permit(:name, :description, :price, :image_url)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
+  get 'uploads/new', to: 'uploads#new'
+  get 'uploads/sign', to: 'uploads#sign'
 
   resources :products do
     resources :cart_products, only: [:create, :destroy]
@@ -19,5 +16,4 @@ Rails.application.routes.draw do
 
   patch "/cart_products/:id/increment", to: "cart_products#increment", as: :increment
   patch "/cart_products/:id/decrement", to: "cart_products#decrement", as: :decrement
-
 end
